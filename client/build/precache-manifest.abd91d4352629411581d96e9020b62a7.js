45146f48544ccaf73e97ce08573baeabcb83b803
B-ry C <doybrian@B-rys-MacBook-Pro.local>
2019-05-06 09:45:33 -0700

    npm run build

---
+++ client/build/precache-manifest.abd91d4352629411581d96e9020b62a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b017315b0e4cee927155",
+    "url": "/static/css/main.57999401.chunk.css"
+  },
+  {
+    "revision": "b017315b0e4cee927155",
+    "url": "/static/js/main.e807e119.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "995303e3321723a03300",
+    "url": "/static/js/2.2858f52d.chunk.js"
+  },
+  {
+    "revision": "0f3d1c99e2e9c85192c89e24622a6593",
+    "url": "/index.html"
+  }
+];
